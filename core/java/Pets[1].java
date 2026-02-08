200c49fe7ad4b8f1a48fa54f68c9f554127239ff
Madhumitha V <madhumithaah06@gmail.com>
2026-02-08 23:13:16 +0530

    Add files via upload

---
+++ core/java/Pets[1].java
@@ -0,0 +1,12 @@
+package PST;
+
+public class Pets {
+	public static void main(String[] args) {
+		animal();
+		
+	}
+	public static void animal() {
+		System.out.println("dog is a pet animal");
+	}
+
+}
