f91b074d513d86fd68eb1f04e31f01dbcd56935f
Madhumitha V <madhumithaah06@gmail.com>
2026-02-08 23:11:58 +0530

    Add files via upload

---
+++ OOPS/JAVA/OOPS[1].java
@@ -0,0 +1,11 @@
+package PST;
+
+public class OOPS {
+	public static void main(String [] args) {
+		method1();}
+		public static  void method1() {
+			System.out.println("hello world!");
+		}
+	}
+
+
