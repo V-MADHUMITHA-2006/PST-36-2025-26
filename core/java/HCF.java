8c26418d198d4f47c5ba00745197f4c8b75c338c
Madhumitha V <madhumithaah06@gmail.com>
2026-02-16 19:02:24 +0530

    Add files via upload

---
+++ core/java/HCF.java
@@ -0,0 +1,25 @@
+package PST;
+import java.util.Scanner;
+public class HCF {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter first number: ");
+	        int num1 = sc.nextInt();
+
+	        System.out.print("Enter second number: ");
+	        int num2 = sc.nextInt();
+
+	        while (num2 != 0) {
+	            int temp = num2;
+	            num2 = num1 % num2;
+	            num1 = temp;
+	        }
+
+	        System.out.println("HCF = " + num1);
+
+	        sc.close();
+	    }
+	}
+
+
