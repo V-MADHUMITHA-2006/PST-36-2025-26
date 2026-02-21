ffae1cf13f756a43d501e89040e485e07bcec1e3
Madhumitha V <madhumithaah06@gmail.com>
2026-02-21 21:59:26 +0530

    Add files via upload

---
+++ core/java/LinearSearch[1].java
@@ -0,0 +1,29 @@
+package PST;
+import java.util.Scanner;
+public class LinearSearch {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        int[] arr = {10, 20, 30, 40, 50};
+
+	        System.out.print("Enter the key to search: ");
+	        int key = sc.nextInt();
+
+	        boolean found = false;
+
+	        for (int i = 0; i < arr.length; i++) {
+	            if (arr[i] == key) {
+	                System.out.println("Element found at index " + i);
+	                found = true;
+	                break;
+	            }
+	        }
+
+	        if (!found) {
+	            System.out.println("Element not found");
+	        }
+
+	        sc.close();
+	    }
+	}
